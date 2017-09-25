6e44e1127a6de78f570742073cebf53c0dfd0b3e
gettindatfoshow <robert.dale.morris@gmail.com>
2017-09-24 23:19:55 -0400

    fixed constraint mapping issues with experience points, program now runs again

--- src/test/java/com/capstone/inventory/capstonedsufall2017RoseRobinventoryproject/model/misc/ExperiencePointTest.java
+++ src/test/java/com/capstone/inventory/capstonedsufall2017RoseRobinventoryproject/model/misc/ExperiencePointTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class ExperiancePointTest {
+public class ExperiencePointTest {
     @Test
     public void getId() throws Exception {
     }
