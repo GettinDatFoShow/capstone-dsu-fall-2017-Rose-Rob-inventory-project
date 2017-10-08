07e96b1879e23dac02714e5c594e8b0d666244f3
gettindatfoshow <robert.dale.morris@gmail.com>
2017-10-08 11:34:34 -0400

    added create file

---
+++ web-app/src/pages/item-create/item-create.ts
@@ -0,0 +1,26 @@
+import { ItemService } from './../../provider/item-service';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ItemCreatePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-item-create',
+  templateUrl: 'item-create.html',
+})
+export class ItemCreatePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public itemService: ItemService) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ItemCreatePage');
+  }
+
+}
