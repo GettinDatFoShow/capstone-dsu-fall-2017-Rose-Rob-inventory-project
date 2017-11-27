2acb9788488362a63f4e5a358781d6ca728356d2
gettindatfoshow <robert.dale.morris@gmail.com>
2017-11-27 14:20:37 -0500

    removed qrscanner plugin

--- src/main/java/com/capstone/inventory/capstonedsufall2017RoseRobinventoryproject/rest/constants/MyIp.java
+++ src/main/java/com/capstone/inventory/capstonedsufall2017RoseRobinventoryproject/rest/constants/MyIp.java
@@ -4,6 +4,6 @@ public class MyIp {
 
     //public static final String IP = "http://192.168.117.35:8100";
     //public static final String IP = "http://192.168.1.10:8100";
-    public static final String IP = "http://192.168.133.142:8100";
+    public static final String IP = "172.31.99.131:8100";
 
 }
