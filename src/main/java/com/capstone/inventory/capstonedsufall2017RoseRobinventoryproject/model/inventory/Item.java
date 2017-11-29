56ee8ee42220119a910f2c0db320285a6d4bee12
gettindatfoshow <robert.dale.morris@gmail.com>
2017-11-29 10:48:39 -0500

    updated the back end item model along with the h2 start up to include an audited date.

--- src/main/java/com/capstone/inventory/capstonedsufall2017RoseRobinventoryproject/model/inventory/Item.java
+++ src/main/java/com/capstone/inventory/capstonedsufall2017RoseRobinventoryproject/model/inventory/Item.java
@@ -43,6 +43,17 @@ public class Item {
     @Column(name="last_updated")
     private String lastUpdated;
 
+    @Column(name="lastAudit")
+    private String lastAudit;
+
+    public String getLastAudit() {
+        return lastAudit;
+    }
+
+    public void setLastAudit(String lastAudit) {
+        this.lastAudit = lastAudit;
+    }
+
     @Column(name="active")
     private Boolean active;
 
@@ -88,6 +99,19 @@ public class Item {
         this.addedToRoom = addedToRoom;
         this.created = created;
         this.lastUpdated = lastUpdated;
+        this.active = active;
+        this.cost = cost;
+        this.isPaid = isPaid;
+    }
+
+    public Item(String specialCode, String type, Room room, String addedToRoom, String created, String lastUpdated, String lastAudit, Boolean active, Double cost, Boolean isPaid) {
+        this.specialCode = specialCode;
+        this.type = type;
+        this.room = room;
+        this.addedToRoom = addedToRoom;
+        this.created = created;
+        this.lastUpdated = lastUpdated;
+        this.lastAudit = lastAudit;
         this.active = active;
         this.cost = cost;
         this.isPaid = isPaid;
@@ -222,52 +246,6 @@ public class Item {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Item item = (Item) o;
-
-        if (id != null ? !id.equals(item.id) : item.id != null) return false;
-        if (specialCode != null ? !specialCode.equals(item.specialCode) : item.specialCode != null) return false;
-        if (description != null ? !description.equals(item.description) : item.description != null) return false;
-        if (color != null ? !color.equals(item.color) : item.color != null) return false;
-        if (type != null ? !type.equals(item.type) : item.type != null) return false;
-        if (room != null ? !room.equals(item.room) : item.room != null) return false;
-        if (addedToRoom != null ? !addedToRoom.equals(item.addedToRoom) : item.addedToRoom != null) return false;
-        if (created != null ? !created.equals(item.created) : item.created != null) return false;
-        if (lastUpdated != null ? !lastUpdated.equals(item.lastUpdated) : item.lastUpdated != null) return false;
-        if (active != null ? !active.equals(item.active) : item.active != null) return false;
-        if (cost != null ? !cost.equals(item.cost) : item.cost != null) return false;
-        if (isPaid != null ? !isPaid.equals(item.isPaid) : item.isPaid != null) return false;
-        if (itemlocation != null ? !itemlocation.equals(item.itemlocation) : item.itemlocation != null) return false;
-        if (details != null ? !details.equals(item.details) : item.details != null) return false;
-        if (histories != null ? !histories.equals(item.histories) : item.histories != null) return false;
-        return images != null ? images.equals(item.images) : item.images == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (specialCode != null ? specialCode.hashCode() : 0);
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (color != null ? color.hashCode() : 0);
-        result = 31 * result + (type != null ? type.hashCode() : 0);
-        result = 31 * result + (room != null ? room.hashCode() : 0);
-        result = 31 * result + (addedToRoom != null ? addedToRoom.hashCode() : 0);
-        result = 31 * result + (created != null ? created.hashCode() : 0);
-        result = 31 * result + (lastUpdated != null ? lastUpdated.hashCode() : 0);
-        result = 31 * result + (active != null ? active.hashCode() : 0);
-        result = 31 * result + (cost != null ? cost.hashCode() : 0);
-        result = 31 * result + (isPaid != null ? isPaid.hashCode() : 0);
-        result = 31 * result + (itemlocation != null ? itemlocation.hashCode() : 0);
-        result = 31 * result + (details != null ? details.hashCode() : 0);
-        result = 31 * result + (histories != null ? histories.hashCode() : 0);
-        result = 31 * result + (images != null ? images.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "Item{" +
                 "id='" + id + '\'' +
@@ -276,9 +254,10 @@ public class Item {
                 ", color='" + color + '\'' +
                 ", type='" + type + '\'' +
                 ", room=" + room +
-                ", addedToRoom=" + addedToRoom +
-                ", created=" + created +
-                ", lastUpdated=" + lastUpdated +
+                ", addedToRoom='" + addedToRoom + '\'' +
+                ", created='" + created + '\'' +
+                ", lastUpdated='" + lastUpdated + '\'' +
+                ", lastAudit='" + lastAudit + '\'' +
                 ", active=" + active +
                 ", cost=" + cost +
                 ", isPaid=" + isPaid +
@@ -288,5 +267,4 @@ public class Item {
                 ", images=" + images +
                 '}';
     }
-
 }
